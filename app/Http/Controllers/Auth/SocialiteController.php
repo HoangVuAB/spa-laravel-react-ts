7270645fb445ae92c0adcd86bbf410668c1e744c
Hoang Vu <hoang.vu@alobridge.com>
2024-01-15 18:08:57 +0700

    Update #004 Update workflow
    Update #004 Update workflow
    
    Update #004 Update workflow
    
    Update #004 Update workflow

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -3,49 +3,29 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Exception;
+use App\Services\AuthService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Str;
-use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    public function socialLogin(string $provider): Response
+    public function __construct(protected AuthService $authService)
     {
-        $redirectUrl = Socialite::driver($provider)->redirect()->getTargetUrl();
-
-        return response('', 409)->header('X-Inertia-Location', $redirectUrl);
     }
 
-    public function handleProviderCallback(string $provider): View|RedirectResponse
+    /*
+    * Redirect to login social account
+    */
+    public function socialLogin(string $provider): Response
     {
-        try {
-            $user = Socialite::driver($provider)->user();
+        return $this->authService->socialLogin($provider);
+    }
 
-            $current_user = User::where($provider.'_id', $user->id)->first();
-
-            if ($current_user) {
-                Auth::login($current_user);
-
-                return redirect()->intended('dashboard');
-            } else {
-                $newUser = User::updateOrCreate(['email' => $user->email], [
-                    'user_name' => $user->name,
-                    $provider.'_id' => $user->id,
-                    'password' => Str::password(),
-                ]);
-
-                Auth::login($newUser);
-
-                return redirect()->intended('dashboard');
-            }
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-        }
+    /*
+    * Handle callback to provider
+    */
+    public function handleProviderCallback(string $provider): RedirectResponse
+    {
+        return $this->authService->handleProviderCallback($provider);
     }
 }
--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -5,13 +5,18 @@ namespace App\Services;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use App\Repositories\User\UserRepository;
+use Exception;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Inertia\Response as InertiaResponse;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthService
 {
@@ -50,6 +55,45 @@ class AuthService
         Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
+    }
+
+    public function socialLogin(string $provider): Response
+    {
+        $redirectUrl = Socialite::driver($provider)->redirect()->getTargetUrl();
+
+        return response('', 409)->header('X-Inertia-Location', $redirectUrl);
+    }
+
+    public function handleProviderCallback(string $provider): RedirectResponse
+    {
+        try {
+            $user = Socialite::driver($provider)->user();
+
+            $current_user = $this->userRepository->findByField($provider.'_id', $user->id);
+
+            if ($current_user) {
+                Auth::login($current_user);
+
+                return redirect()->intended('dashboard');
+            } else {
+                $newUser = $this->userRepository->updateOrCreate(
+                    ['email' => $user->email],
+                    [
+                        'user_name' => $user->name,
+                        $provider.'_id' => $user->id,
+                        'password' => Str::password(),
+                    ]
+                );
+
+                Auth::login($newUser);
+
+                return redirect()->intended('dashboard');
+            }
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+
+            return redirect()->route('login');
+        }
     }
 
     public function useTerms(): InertiaResponse
